6f42a1d43e385e285658445bfb3d373f843b4385
Ryan <ryan.o.mackey@gmail.com>
2016-09-14 10:48:24 -0600

    filter feature complete

--- client/src/components/LibraryGame.js
+++ client/src/components/LibraryGame.js
@@ -40,24 +40,26 @@ export default class LibraryGame extends React.Component {
     });
     return (
       <div className="row">
-      {library.library.map((game) => {
-        return (
-          <div key={game.game_id} className="col s12 m4 l3" style={{height:'500px',marginTop:'1%',marginBottom:'1%'}}>
-            <div className="card search-result">
-              <div className="card-image" style={{height:'350px', display:'flex',alignItems:'center'}}>
-                <img className="activator" src={game.game_image}/>
+        {library.library.map((game) => {
+          {if (game.is_visible) {
+            return (
+              <div key={game.game_id} className="col s12 m4 l3" style={{height:'500px',marginTop:'1%',marginBottom:'1%'}}>
+                <div className="card search-result">
+                  <div className="card-image" style={{height:'350px', display:'flex',alignItems:'center'}}>
+                    <img className="activator" src={game.game_image}/>
+                  </div>
+                  <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">{game.game_name}</span>
+                  </div>
+                  <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4"><i className="material-icons right">close</i>{game.game_name}</span>
+                    <p>{game.game_deck}</p>
+                  </div>
+                </div>
               </div>
-              <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">{game.game_name}</span>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4"><i className="material-icons right">close</i>{game.game_name}</span>
-                <p>{game.game_deck}</p>
-              </div>
-            </div>
-          </div>
-        )
-      })}
+            )
+          }}
+        })}
       </div>
     )
   }
